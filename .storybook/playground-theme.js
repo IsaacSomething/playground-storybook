a21ac534a01379057ee5d8e43c3a46f438f63c8f
Timothy <tcuthbert@dvt.co.za>
2022-11-01 14:33:30 +0200

    ^ assets & readme

--- .storybook/playground-theme.js
+++ .storybook/playground-theme.js
@@ -4,7 +4,7 @@ export default create({
   base: "light",
   brandTitle: "Playground storybook",
   brandUrl: "https://material.angular.io",
-  brandImage: "https://img-c.udemycdn.com/course/240x135/1442960_e7f1_2.jpg",
+  brandImage: "assets/material.svg",
   brandTarget: "_self",
   fontBase: '"Roboto", sans-serif',
 });
