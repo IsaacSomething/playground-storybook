cb2ce7d074f79cc675385b0215a92139c2cc8579
Timothy <tcuthbert@dvt.co.za>
2022-11-02 13:03:44 +0200

    + figma integratio

--- src/stories/components/button/button.stories.ts
+++ src/stories/components/button/button.stories.ts
@@ -21,14 +21,28 @@ const Template: Story<Button> = (args: Button) => ({
   },
 });
 
+const urlPrefix =
+  'https://www.figma.com/file/B4I9c9XojvfCuHyqlegRWg/material?node-id=';
+
+const designParams = {
+  type: 'figma',
+  url: `${urlPrefix}?node-id=0%3A1`,
+};
+
 export const Default = Template.bind({});
 Default.args = {
   type: 'default',
+};
+Default.parameters = {
+  design: { ...designParams, url: `${urlPrefix}0%3A1` },
 };
 
 export const Flat = Template.bind({});
 Flat.args = {
   type: 'flat',
+};
+Flat.parameters = {
+  design: { ...designParams, url: `${urlPrefix}1%3A391` },
 };
 
 export const Raised = Template.bind({});
